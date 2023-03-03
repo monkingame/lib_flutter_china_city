f3251302bb942d0d09088ff6cc02365700f2a37a
sunshouhua <sunshouhua@gmail.com>
2023-03-03 21:41:21 +0800

    main

--- china_city_selector/example/lib/main.dart
+++ china_city_selector/example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-import 'home.dart';
+import 'package:china_city_selector/area_analyzer.dart';
+import 'package:china_city_selector/area_result.dart';
+import 'package:china_city_selector/selector_area.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,46 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('china cities')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Center(
+            child: OutlinedButton(
+              onPressed: () async {
+                final nation = AreaAnalyzer.analyzeNation();
+                final provinces = nation.allProvinces.values.toList();
+                provinces.sort((a, b) => a.value.compareTo(b.value));
+                // print(provinces);
+              },
+              child: const Text('print all provinces'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: SelectorArea(
+              onNewArea: (AreaResult newArea) async {
+                print(newArea);
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
