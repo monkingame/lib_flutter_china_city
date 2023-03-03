23ad49bf99c1a4158568b8a24901d15a44eb8d5a
sunshouhua <65739056@qq.com>
2023-03-03 09:46:56 +0800

    refactor

--- preview_test/lib/selector_area.dart
+++ preview_test/lib/selector_area.dart
@@ -18,47 +18,34 @@ class SelectorArea extends StatefulWidget {
 class _SelectorAreaState extends State<SelectorArea> {
   final nation = AreaAnalyzer.analyzeNation();
 
-  List<String> _provinces = [];
-  List<String> _cities = [];
-  List<String> _counties = [];
+  List<String> _provinceNames = [];
+  List<String> _cityNames = [];
+  List<String> _countyNames = [];
 
-  String? _industry;
-  String? _currProvince;
-  String? _currCity;
-  String? _currCounty;
+  // String? _industry;
+  String? _provinceName;
+  String? _cityName;
+  String? _countyName;
 
-  List<String?> get _industryValues => [null, ..._industries];
+  // List<String?> get _industryValues => [null, ..._industries];
 
-  List<String?> get _provinceValues => [null, ..._provinces];
-  List<String?> get _cityValues => [null, ..._cities];
-  List<String?> get _countyValues => [null, ..._counties];
+  List<String?> get _provinceDropNames => [null, ..._provinceNames];
+  List<String?> get _cityDropNames => [null, ..._cityNames];
+  List<String?> get _countyDropNames => [null, ..._countyNames];
 
   Text _getText(String? s) => (s == null) ? const Text('请选择') : Text(s);
 
   @override
+  void initState() {
+    final provinces = nation.allProvinces.values.toList();
+    provinces.sort((a, b) => a.value.compareTo(b.value));
+    _provinceNames = provinces.map((e) => e.name).toList();
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // final dropdown = DropdownButton<String>(
-    //   value: _industry,
-    //   items: _industryValues
-    //       .map<DropdownMenuItem<String>>(
-    //         (value) => DropdownMenuItem<String>(
-    //           value: value,
-    //           child: _getText(value),
-    //         ),
-    //       )
-    //       .toList(),
-    //   onChanged: (value) async {
-    //     setState(() {
-    //       _industry = value;
-    //     });
-
-    //     final onChange = widget.onNewString;
-    //     if (onChange != null) onChange(value);
-    //   },
-    // );
-
-    // return DropdownButtonHideUnderline(child: dropdown);
-
     final row = Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
@@ -72,8 +59,8 @@ class _SelectorAreaState extends State<SelectorArea> {
 
   Widget _buildProvince() {
     final dropdown = DropdownButton<String>(
-      value: _currProvince,
-      items: _provinceValues
+      value: _provinceName,
+      items: _provinceDropNames
           .map<DropdownMenuItem<String>>(
             (value) => DropdownMenuItem<String>(
               value: value,
@@ -83,7 +70,15 @@ class _SelectorAreaState extends State<SelectorArea> {
           .toList(),
       onChanged: (value) async {
         setState(() {
-          _currProvince = value;
+          _provinceName = value;
+
+          if (_provinceName != null) {
+          } else {
+            _cityName = null;
+            _countyName = null;
+            _cityNames = [];
+            _countyNames = [];
+          }
         });
 
         // final onChange = widget.onNewString;
@@ -96,8 +91,8 @@ class _SelectorAreaState extends State<SelectorArea> {
 
   Widget _buildCity() {
     final dropdown = DropdownButton<String>(
-      value: _currCity,
-      items: _cityValues
+      value: _cityName,
+      items: _cityDropNames
           .map<DropdownMenuItem<String>>(
             (value) => DropdownMenuItem<String>(
               value: value,
@@ -107,7 +102,13 @@ class _SelectorAreaState extends State<SelectorArea> {
           .toList(),
       onChanged: (value) async {
         setState(() {
-          _currCity = value;
+          _cityName = value;
+
+          if (_cityName != null) {
+          } else {
+            _countyName = null;
+            _countyNames = [];
+          }
         });
 
         // final onChange = widget.onNewString;
@@ -120,8 +121,8 @@ class _SelectorAreaState extends State<SelectorArea> {
 
   Widget _buildCounty() {
     final dropdown = DropdownButton<String>(
-      value: _currCounty,
-      items: _countyValues
+      value: _countyName,
+      items: _countyDropNames
           .map<DropdownMenuItem<String>>(
             (value) => DropdownMenuItem<String>(
               value: value,
@@ -131,7 +132,7 @@ class _SelectorAreaState extends State<SelectorArea> {
           .toList(),
       onChanged: (value) async {
         setState(() {
-          _currCounty = value;
+          _countyName = value;
         });
 
         // final onChange = widget.onNewString;
@@ -143,34 +144,34 @@ class _SelectorAreaState extends State<SelectorArea> {
   }
 }
 
-const _industries = [
-  '国有企业',
-  '省委市委',
-  '纪委',
-  '政法',
-  '发改委',
-  '教育',
-  '科学技术',
-  '工业和信息化',
-  '公安',
-  '交警',
-  '高速交警',
-  '民政',
-  '司法',
-  '税务',
-  '人力资源和社会保障',
-  '国土海洋',
-  '生态环境',
-  '住建',
-  '交通运输',
-  '水利水务',
-  '农业农村',
-  '文化和旅游',
-  '卫生健康',
-  '应急管理',
-  '审计',
-  '国资委',
-  '市场监督管理局',
-  '大数据局',
-  '其他',
-];
+// const _industries = [
+//   '国有企业',
+//   '省委市委',
+//   '纪委',
+//   '政法',
+//   '发改委',
+//   '教育',
+//   '科学技术',
+//   '工业和信息化',
+//   '公安',
+//   '交警',
+//   '高速交警',
+//   '民政',
+//   '司法',
+//   '税务',
+//   '人力资源和社会保障',
+//   '国土海洋',
+//   '生态环境',
+//   '住建',
+//   '交通运输',
+//   '水利水务',
+//   '农业农村',
+//   '文化和旅游',
+//   '卫生健康',
+//   '应急管理',
+//   '审计',
+//   '国资委',
+//   '市场监督管理局',
+//   '大数据局',
+//   '其他',
+// ];
